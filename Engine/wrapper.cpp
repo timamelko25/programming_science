8d5503b28beec66f086078097bef77817f329123
t.melko <timamelko25@gmail.com>
2024-12-03 20:49:13 +0300

    correct wrapper

--- Engine/wrapper.cpp
+++ Engine/wrapper.cpp
@@ -8,26 +8,6 @@
 using InputArgs = std::vector<std::pair<std::string, std::string>>;
 using ArgsMap = std::vector<std::pair<std::string, std::string>>;
 
-template <typename Subject, typename Method, std::size_t... I>
-std::string Wrapper::func_caller(Subject* subject, Method method, const std::vector<std::string>& args, std::index_sequence<I...>) {
-    return (subject->*method)(args[I]...);
-}
-
-template<typename Subject, typename... Args>
-Wrapper::Wrapper(Subject* subject, std::string(Subject::* method)(Args...), const InputArgs& input_args) {
-    if (input_args.size() != sizeof...(Args)) {
-        std::cerr << "Wrong number of arguments." << std::endl;
-    }
-
-    for (const auto& arg : input_args) {
-        argsTitles.push_back(arg.first);
-    }
-
-    function = [this, subject, method](const std::vector<std::string>& argsVector) {
-        return func_caller(subject, method, argsVector, std::index_sequence_for<Args...>{});
-    };
-}
-
 std::string Wrapper::execute(const ArgsMap& args_map) {
     if (args_map.size() != argsTitles.size()) {
         std::cerr << "Wrong number of arguments." << std::endl;
